d0f6312767dffa3e829f1cee3f623ff8e9c143fc
Eleani Pita <eleanipita@MacBook-Pro-de-Miguel.local>
2019-04-22 15:47:06 -0400

    bootstrap/style

---
+++ assets/code.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyD9Y-rCQdBBPsSS8DPU3alY7vTXy9eLpi0",
+    authDomain: "train-station-67695.firebaseapp.com",
+    databaseURL: "https://train-station-67695.firebaseio.com",
+    projectId: "train-station-67695",
+    storageBucket: "train-station-67695.appspot.com",
+    messagingSenderId: "1033753470572"
+  };
+  firebase.initializeApp(config);
