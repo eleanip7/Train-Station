c3505c14213cf2293597cda9cb21ed45aec8a94b
Eleani Pita <eleanipita@MacBook-Pro-de-Miguel.local>
2019-04-24 12:42:05 -0400

    database.ref.push new train data from object

--- assets/code.js
+++ assets/code.js
@@ -1,29 +1,53 @@
- // Initialize Firebase
- var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyD9Y-rCQdBBPsSS8DPU3alY7vTXy9eLpi0",
     authDomain: "train-station-67695.firebaseapp.com",
     databaseURL: "https://train-station-67695.firebaseio.com",
     projectId: "train-station-67695",
     storageBucket: "train-station-67695.appspot.com",
     messagingSenderId: "1033753470572"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  //button for adding employees
-  $("#add-train-btn").on("click", function (event){ 
-      console.log(function1)
-      event.preventDefault();
+var database = firebase.database();
 
-      //grab user input
-      var trainName = $("#train-name-input").val().trim();
-      var destination = $("#destination-input").val().trim();
-      var firstTrain = moment($("#first-train-input").val().trim(), "MM:HH").format("X");
-      var frequency = $("#frequency-input").val().trim();
+//initial values
 
-      var newTrain = {
+var nextArrival = 0;
+var minsAway = 0;
+
+//button for adding employees
+$("#add-train-btn").on("click", function (event) {
+    console.log("HI")
+    event.preventDefault();
+
+    //grab user input
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var firstTrain = $("#first-train-input").val().trim();
+    var frequency = parseInt($("#frequency-input").val().trim());
+
+// object that holds temporary data
+    var newTrain = {
         name: trainName,
         destination: destination,
-        firstTrain: firstTrain,
         frequency: frequency,
-      }
-  });
+        dataMins: minsAway,
+        dataArriv: nextArrival
+    };
+
+    //Uploads to table train data
+    database.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrain);
+    console.log(newTrain.frequency);
+
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-input").val("");
+    $("#frequency-input").val("");
+});
+
